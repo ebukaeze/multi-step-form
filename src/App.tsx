f0b386cd4a7b476454913070db62588f56dfb172
Ebuka Ezenwabasili <wizzyblaq94@gmail.com>
2024-01-16 14:33:24 +0100

    Ci: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="w-full flex items-center justify-center">
+        <h1 className="text-4xl font-bold text-red-500">Hello TailwindCSS</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
