61bf3790217a8a85187a660f5c66006b4a201272
Ebuka Ezenwabasili <wizzyblaq94@gmail.com>
2024-03-17 12:38:11 +0100

    refactor: added custom stylings to tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,10 @@
 module.exports = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
   ],
   prefix: "",
   theme: {
@@ -17,7 +17,21 @@ module.exports = {
       },
     },
     extend: {
+      backgroundImage: {
+        "sidebar-svg": "url('./src/assets/images/bg-sidebar-desktop.svg')",
+        "sidebar-mobile": "url('./src/assets/images/bg-sidebar-mobile.svg')",
+      },
       colors: {
+        "marine-blue": "hsl(213, 96%, 18%)",
+        "purplish-blue": "hsl(243, 100%, 62%)",
+        "pastel-blue": "hsl(228, 100%, 84%)",
+        "light-blue": "hsl(206, 94%, 87%)",
+        "strawberry-red": "hsl(354, 84%, 57%)",
+        "cool-gray": "hsl(231, 11%, 63%)",
+        "light-gray": "hsl(229, 24%, 87%)",
+        magnolia: "hsl(217, 100%, 97%)",
+        alabaster: "hsl(231, 100%, 99%)",
+        white2: "hsl(0, 0%, 100%)",
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
         ring: "hsl(var(--ring))",
@@ -74,4 +88,4 @@ module.exports = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-}
+};
